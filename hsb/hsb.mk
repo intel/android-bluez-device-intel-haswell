e6d3aab5d222589a399da7f36ebbc9b09183d1e9
Anders Nilsson <anders.nilsson@intel.com>
2013-09-23 10:00:32 +0200

    hsb: use haswell power mixin
    Change-Id: I842beebe92996fe8e2eda6cc5dcda6458c2a7e84
    Depends-Change-Id: I64ec3202c912702a93b3ace3f54d617821031084
    Issue: ABRO-96

--- hsb/hsb.mk
+++ hsb/hsb.mk
@@ -26,4 +26,5 @@ $(call inherit-mixin, ethernet, static)
 $(call inherit-mixin, storage, sdcard)
 $(call inherit-mixin, fastboot, userfastboot)
 $(call inherit-mixin, audio, pc)
+$(call inherit-mixin, power, hsw)
 
