96c638b1c21299c500b2157210e6eb9b8645151d
Andrew Boie <andrew.p.boie@intel.com>
2013-10-03 14:26:18 -0700

    hsb: use generic EFI releasetools extensions
    Issue: ADEV-21
    Depends-Change-Id: I05faef1f1bc5eda7f2c1b36a1f0857962fd0e319
    Change-Id: I67949e5783f6c3fa52748dacb8e254776f3fb318
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- hsb/hsb.mk
+++ hsb/hsb.mk
@@ -11,6 +11,12 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.rc:root/init.$(TARGET_PRODUCT).rc \
 	$(LOCAL_PATH)/init.recovery.rc:root/init.recovery.$(TARGET_PRODUCT).rc
 
+# Releasetools extensions for updating EFI System Partition and
+# userfastboot (if present). Product teams will need to copy this
+# file and make their own changes to it if they have additional
+# OTA tasks; there currently can only be one of these.
+TARGET_RELEASETOOLS_EXTENSIONS := device/intel/common/releasetools/releasetools-generic-efi.py
+
 $(call inherit-mixin, gms, false)
 $(call inherit-mixin, houdini, true)
 $(call inherit-mixin, boot-arch, efi)
