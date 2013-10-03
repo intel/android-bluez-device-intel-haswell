3a72ef281f82f0478517279eb2958a6294757e24
Johan Mossberg <johan.mossberg@intel.com>
2013-10-02 17:26:50 -0700

    Set display density for hsb
    Change-Id: Ie64325c1edc874ac0c90a8fc9d76b22e28e42ec7

--- hsb/hsb.mk
+++ hsb/hsb.mk
@@ -14,6 +14,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-mixin, gms, false)
 $(call inherit-mixin, houdini, true)
 $(call inherit-mixin, boot-arch, efi)
+$(call inherit-mixin, display-density, medium)
 $(call inherit-mixin, graphics, ufo)
 $(call inherit-mixin, ethernet, static)
 $(call inherit-mixin, storage, sdcard)
