bae3756bcc41b3ee942dad7017260c16ce1ba91d
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2013-12-17 12:18:32 -0800

    Add thermal-conf.xml and mixin for thermal-daemon
    Added per platform tunable thermal-conf.xml to haswell.
    Refer to man pages under thermal-daemon for format.
    
    Also added thermal-daemon as a mixin for thermals.
    
    For: APDEV-757
    Change-Id: I643fce1f0bc4ba4d248a9f40352128e4d783d936
    Depends-Change-Id: I0a72da1b22a6e0724a1471d88b16f241264da871
    Depends-Change-Id: I74b5240c0562e36a3864980e26ecc0aa6fdcfb28
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>

--- haswell.mk
+++ haswell.mk
@@ -7,7 +7,8 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/init.haswell.rc:root/init.haswell.rc \
 	$(LOCAL_PATH)/init.recovery.haswell.rc:root/init.recovery.haswell.rc \
 	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+	$(LOCAL_PATH)/thermal-conf.xml:system/etc/thermal-daemon/thermal-conf.xml
 
 $(call inherit-mixin, cpu-arch, hsw)
 
